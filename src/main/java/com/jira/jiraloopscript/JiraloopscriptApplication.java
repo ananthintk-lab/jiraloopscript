0d2e495f83adf87100fb1f20b178e3995eff6f4c
ananthintk-lab <ananthintk@gmail.com>
2026-06-26 15:31:55 +0530

    initial commit

---
+++ src/main/java/com/jira/jiraloopscript/JiraloopscriptApplication.java
@@ -0,0 +1,13 @@
+package com.jira.jiraloopscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraloopscriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraloopscriptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jira/jiraloopscript/JiraloopscriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jira.jiraloopscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraloopscriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
